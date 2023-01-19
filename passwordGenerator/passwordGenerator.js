ccb5f82b42403c0f11fe04dfc6c39df5d41ebf89
M Nandagopan <madhunandavanam1@gmail.com>
2023-01-19 12:23:29 +0530

    minor edits to passGen js and html

--- passwordGenerator/passwordGenerator.js
+++ passwordGenerator/passwordGenerator.js
@@ -2,12 +2,12 @@
 console.log("javascript started");
 console.log("''''''''''''''''''");
 
-try{
-    let ps1 = document.getElementById("pswrd1");
-    let ps2 = document.getElementById("pswrd2");
-    console.log("ps1 is : " + ps1);
-    console.log("ps2 is : " + ps2);
-}catch(err){console.log("error is : "+err.message);}
+// try{
+//     let ps1 = document.getElementById("pswrd1");
+//     let ps2 = document.getElementById("pswrd2");
+//     console.log("ps1 is : " + ps1);
+//     console.log("ps2 is : " + ps2);
+// }catch(err){console.log("error is : "+err.message);}
 
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
@@ -23,23 +23,38 @@ console.log("small alphabets : " + sA);
 console.log("numbers : " + n);
 console.log("special charac : " + s);
 
-let cASum = 0;
-let sASum = 0;
-let nSum = 0;
-let sSum = 0;
+
 
 function genPass(){
 
     try{
+        let ps1 = document.getElementById("pswrd1");
+        let ps2 = document.getElementById("pswrd2");
+        console.log("ps1 is : " + ps1);
+        console.log("ps2 is : " + ps2);
         ps1.textContent = callPasswrd();
+        console.log("ps1 text content is : " + ps1.textContent);
+        console.log(" ");
+        console.log(" ");
+        console.log(" ");
+        console.log(" ");
+        console.log(" ");
         ps2.textContent = callPasswrd();
+        console.log("ps2 text content is : " + ps1.textContent);
     } catch(err){console.log(err.message);}
     
 }
 
 function callPasswrd(){
 
+
     try{
+
+        let cASum = 0;
+        let sASum = 0;
+        let nSum = 0;
+        let sSum = 0;
+
         console.log("callPasswrd() called")
         let wrd = ""
         let choice = 0;
